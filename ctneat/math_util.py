e9d25f43becf393330307cf9384f0727c53669a9
Sergiy Horef <s@unzim.com>
2025-09-15 12:56:26 +0300

    rewrote math_util to use numpy

--- ctneat/math_util.py
+++ ctneat/math_util.py
@@ -1,49 +1,39 @@
-"""Commonly used functions not available in the Python2 standard library."""
+"""
+Commonly used functions not available in the Python2 standard library.
+Currently only maintained for backwards compatibility.
+"""
 
-from math import sqrt, exp
+from math import sqrt
+import numpy as np
 
 
 def mean(values):
-    values = list(values)
-    return sum(map(float, values)) / len(values)
+    return np.mean(values).item()
 
 
 def median(values):
-    values = list(values)
-    values.sort()
-    return values[len(values) // 2]
+    return np.median(values).item()
 
 
 def median2(values):
-    values = list(values)
-    n = len(values)
-    if n <= 2:
-        return mean(values)
-    values.sort()
-    if (n % 2) == 1:
-        return values[n // 2]
-    i = n // 2
-    return (values[i - 1] + values[i]) / 2.0
+    return np.median(values).item()
 
 
 def variance(values):
-    values = list(values)
-    m = mean(values)
-    return sum((v - m) ** 2 for v in values) / len(values)
+    return np.var(values).item()
 
 
 def stdev(values):
-    return sqrt(variance(values))
+    return np.std(values).item()
 
 
 def softmax(values):
     """
     Compute the softmax of the given value set, v_i = exp(v_i) / s,
     where s = sum(exp(v_0), exp(v_1), ..)."""
-    e_values = list(map(exp, values))
-    s = sum(e_values)
-    inv_s = 1.0 / s
-    return [ev * inv_s for ev in e_values]
+    x = np.asarray(values)
+    exps = np.exp(x - np.max(x))
+    return list((exps / np.sum(exps)).item())
 
 
 # Lookup table for commonly used {value} -> value functions.
